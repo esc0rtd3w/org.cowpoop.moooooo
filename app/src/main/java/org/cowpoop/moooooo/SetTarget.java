464ad10dd3e09efde30363df64f74f12fc97d03f
Jason Borowski <esc0rtd3w@gmail.com>
2017-01-16 06:16:35 -0500

    Added Some Java Classes

---
+++ app/src/main/java/org/cowpoop/moooooo/SetTarget.java
@@ -0,0 +1,13 @@
+package org.cowpoop.moooooo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetTarget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_target);
+    }
+}
