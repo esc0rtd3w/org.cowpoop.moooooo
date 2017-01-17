29decebd361ced19fef7486f162b5c97b0e23589
Jason Borowski <esc0rtd3w@gmail.com>
2017-01-17 18:13:08 -0500

    Updated Text

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ jstring
 Java_org_cowpoop_moooooo_Moooooo_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "COW Poop\n\nA Collection of Vulnerabilities Based Around The CVE-2016-5195 Exploit";
     return env->NewStringUTF(hello.c_str());
 }
